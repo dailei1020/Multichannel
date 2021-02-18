0ed3e66131925c3e1fdd2298f2582a5a60992e80
dailei <dailei@coocaa.com>
2021-02-18 18:55:49 +0800

    多渠道案例

---
+++ app/src/banana/java/com/dl/multi_channel/Banana.kt
@@ -0,0 +1,10 @@
+package com.dl.multi_channel
+
+/**
+ * description:
+ * Created by dailei on 2021/2/18
+ *
+ */
+class Banana :Fruits() {
+    val color = "yellow"
+}
---
+++ app/src/main/java/com/dl/multi_channel/Fruits.kt
@@ -0,0 +1,15 @@
+package com.dl.multi_channel
+
+import android.util.Log
+
+/**
+ * description:
+ * Created by dailei on 2021/2/18
+ *
+ */
+open class Fruits {
+
+    open fun print(){
+        Log.d("Fruits","I am Fruits")
+    }
+}
